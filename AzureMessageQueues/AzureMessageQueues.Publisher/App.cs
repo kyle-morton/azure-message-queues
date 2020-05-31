dc64c44fc64cd0a4d884d5c1c43abea16f087fd8
Kyle Morton <kylemortondevelopment@gmail.com>
2020-05-30 19:43:08 -0500

    init

---
+++ AzureMessageQueues/AzureMessageQueues.Publisher/App.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureMessageQueues.Publisher
+{
+    public class App
+    {
+        public async Task Run()
+        {
+
+            // connect to azure queue and wait for new messages
+
+
+        }
+    }
+}
